0e93efb81f48ad96f8754698dbd0349fb06ade49
ingamostova <ingo4ka20041997@gmail.com>
2022-11-04 00:08:11 +0200

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[type="email"]');
+const textarea = document.querySelector('[name="message"]');
+// console.log(form);
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+onPageUpdate();
+
+const object = {};
+// console.log(object);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log(object);
+}
+
+function onFormInput(evt) {
+  object.email = email.value;
+  object.message = textarea.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(object));
+  //   console.log(localStorage);
+}
+
+function onPageUpdate() {
+  const savedValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  //   console.log(savedValue.email);
+  if (savedValue) {
+    email.value = savedValue.email;
+    textarea.value = savedValue.message;
+  }
+}
