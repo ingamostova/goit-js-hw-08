67b0537a4bcfb2b374f4bf95d774182719eeac4a
ingamostova <ingo4ka20041997@gmail.com>
2022-11-04 02:01:57 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+onPageUpdate();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+  //   console.log(evt.seconds);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(evt.seconds));
+}
+
+function onPageUpdate(evt) {
+  const savedTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  //   console.log(savedTime);
+  player
+    .setCurrentTime(savedTime)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,13 +6,12 @@ const textarea = document.querySelector('[name="message"]');
 // console.log(form);
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+const object = {};
+
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 onPageUpdate();
-
-const object = {};
-// console.log(object);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
@@ -24,13 +23,14 @@ function onFormSubmit(evt) {
 function onFormInput(evt) {
   object.email = email.value;
   object.message = textarea.value;
+  //   object[evt.target.name] = evt.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(object));
   //   console.log(localStorage);
 }
 
 function onPageUpdate() {
   const savedValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  //   console.log(savedValue.email);
+  //   console.log(savedValue);
   if (savedValue) {
     email.value = savedValue.email;
     textarea.value = savedValue.message;
